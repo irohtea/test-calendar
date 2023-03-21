c5344d335deb99f74fb931f2fc04840f94643cee
Vladymyr Cherno <107193515+irohtea@users.noreply.github.com>
2023-03-21 21:12:06 +0200

    config upd

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/test-calendar/',
 })
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ?
-     '/calendar/' :
-     '/'
-}
